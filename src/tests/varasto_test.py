14b18fad86b42ac1de0bb4444f9b335bab086903
petrovaana <143181789+petrovaana@users.noreply.github.com>
2025-10-29 12:05:42 +0200

    Back to beginning
    The prior version did not work, so I decided to restart

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -13,19 +13,6 @@ class TestVarasto(unittest.TestCase):
     def test_uudella_varastolla_oikea_tilavuus(self):
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
 
-    def test_konstruktori_negatiivinen_tilavuus(self):
-        v = VarastOo(-5)
-        self.assertAlmostEqual(v.tilavuus, 0)
-        self.assertAlmostEqual(v.saldo, 0)
-
-    def test_konstruktori_alku_saldo_negatiivinen(self):
-        v = Varasto(10, -3)
-        self.assertAlmostEqual(v.saldo, 0)
-
-    def test_kosntruktori_alku_saldo_suurempi_kuin_tilavuus(self):
-        v = Varasto(10, 15)
-        self.assertAlmostEqual(v.saldo, 10)
-
     def test_lisays_lisaa_saldoa(self):
         self.varasto.lisaa_varastoon(8)
 
@@ -36,14 +23,6 @@ class TestVarasto(unittest.TestCase):
 
         # vapaata tilaa pitäisi vielä olla tilavuus-lisättävä määrä eli 2
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 2)
-
-    def test_lisaa_liikaa_varastoon(self):
-        self.varasto.lisaa_varastoon(15)
-        self.assertAlmostEqual(self.varasto.saldo, 10)
-
-    def test_lisaa_negatiivinen_maara(self):
-        self.varasto.lisaa_varastoon(-5)
-        self.assertAlmostEqual(self.varasto.saldo, 0)
 
     def test_ottaminen_palauttaa_oikean_maaran(self):
         self.varasto.lisaa_varastoon(8)
@@ -59,18 +38,4 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
-
-
-    def test_ota_liikaa_varastosta(self):
-        self.varasto.lisaa_varastoon(5)
-        saatu = self.varasto.ota_varastosta(10)
-        self.assertAlmostEqual(saatu, 5)
-        self.assertAlmostEqual(self.varasto.saldo, 0)
-
-
-    def test_ota_negatiivinen_maara(self):
-        self.varasto.lisaa_varastoon(5)
-        saatu = self.varasto.ota_varastosta(-3)
-        self.assertAlmostEqual(saatu, 0)
-        self.assertAlmostEqual(self.varasto.saldo, 5)
         
